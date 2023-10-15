e4221526c45a95b4936dfab14744494a057130de
Victor Martins <victor.martins00@outlook.com>
2023-10-15 16:04:13 -0300

    Unexpected erro on create scaffolding file

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,5 +1,7 @@
-﻿namespace SalesWebMvc.Models {
-    public class Department {
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
         public int Id { get; set; }
         public string Name { get; set; }
     }
