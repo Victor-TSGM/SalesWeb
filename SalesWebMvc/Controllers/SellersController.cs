f2d60cef378774c43c59b6845935b4a821192ed7
Victor Martins <victor.martins00@outlook.com>
2023-11-20 19:43:45 -0300

    feat: Should be able to delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -2,34 +2,63 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _service;
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService service)
+        public SellersController(SellerService service, DepartmentService departmentService)
         {
-            _service = service;
+            _sellerService = service;
+            _departmentService = departmentService;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var list = _service.FindAll();
+            var list = _sellerService.FindAll();
             return View(list);
         }
 
         public IActionResult Create() {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { 
+                Departments = departments
+            };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
-            _service.Insert(seller);
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null) { 
+                return NotFound();
+            }
+
+            var seller = _sellerService.FindById(id);
+
+            if(seller == null) return NotFound();
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
